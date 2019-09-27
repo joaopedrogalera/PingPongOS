3379bb0cda7288168c73d3dd2aaea49f9b65e7ed
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2019-09-26 23:40:07 -0300

    Adicionando #ifdef DEBUG

--- p02/pingpong.c
+++ p02/pingpong.c
@@ -4,25 +4,29 @@
 
 #define STACKSIZE 32768
 
-int tid;
-task_t taskMain;
-task_t *runningTask;
+int tid; //Salva numero de tarefas
+task_t taskMain; //Task da tarefa Main
+task_t *runningTask; //Ponteiro para task em execução
 
 // Inicializa o sistema operacional; deve ser chamada no inicio do main()
 void pingpong_init(){
   setvbuf(stdout, 0, _IONBF, 0);
 
-  tid = 0;
+  tid = 0; //Por enquanto, apenas main
 
+  //Inicializa variaveis da Task Main
   taskMain.prev = NULL;
   taskMain.next = NULL;
   taskMain.tid = 0;
 
+  //Main em execução
   runningTask = &taskMain;
 }
 
 // Cria uma nova tarefa. Retorna um ID> 0 ou erro.
 int task_create (task_t *task, void (*start_func)(void *), void *arg){
+
+  //Criação padrão de novo contexto
   char *stack;
 
   getcontext(&(task->contextTask));
@@ -42,15 +46,24 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg){
 
   makecontext(&(task->contextTask),(void*)(*start_func),1,arg);
 
+  //Soma 1 ao numero de tarefas criado e inicializa variaveis da tarefa
   tid++;
   task->prev = NULL;
   task->next = NULL;
   task->tid = tid;
 
+  #ifdef DEBUG
+    printf("task_create: Criando tarefa %d",tid);
+  #endif
+
   return(tid);
 }
 
 int task_switch (task_t *task){
+  #ifdef DEBUG
+    printf("task_switch: trocando contexto %d->%d",runningTask->tid,task->tid);
+  #endif
+  //Salva o contexto atual na struct da tarefa em execução, altera o ponteiro runningTask para a task e restaura o contexto salvo em task
   task_t *taskAux;
   taskAux = runningTask;
   runningTask = task;
@@ -59,6 +72,10 @@ int task_switch (task_t *task){
 }
 
 void task_exit (int exitCode){
+  //Salva o contexto atual na struct da tarefa em execução, altera o ponteiro runningTask para a task Main e restaura o contexto salvo na task Main
+  #ifdef DEBUG
+    printf("task_exit: tarefa %d sendo encerrada",runningTask->tid);
+  #endif
   task_t *taskAux;
   taskAux = runningTask;
   runningTask = &taskMain;
@@ -66,5 +83,6 @@ void task_exit (int exitCode){
 }
 
 int task_id (){
+  //Retorna o tid da task em execução
   return (runningTask->tid);
 }
