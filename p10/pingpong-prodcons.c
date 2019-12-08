360c83765b22c540179e65e3ba03c67aa41dbcc0
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2019-12-07 21:47:10 -0300

    p10 pronto

--- p10/pingpong-prodcons.c
+++ p10/pingpong-prodcons.c
@@ -2,44 +2,82 @@
 #include <stdlib.h>
 #include "pingpong.h"
 
-semaphore_t *s_buffer, *s_vaga, *s_item;
-int buffer[5];
-char bufferIndex;
+#define NUMSTEPS 60
 
-void produtor(){
-  int item;
-  while (1) {
+semaphore_t s_buffer, s_vaga, s_item;
+task_t p1, p2, p3, c1, c2, c3;
+
+typedef struct item_t{
+  struct buffer_t *prev,*next;
+  int valor;
+}item_t;
+
+item_t *buffer;
+
+void produtor(void *arg){
+  item_t *item;
+  int i;
+  printf("%s início\n",(char*)arg);
+  for(i=0;i<NUMSTEPS/3;i++) {
     task_sleep(1);
-    item = rand()%99;
-    sem_down(s_vaga);
-    sem_down(s_buffer);
-    buffer[bufferIndex] = item;
-    bufferIndex++;
-    sem_up(s_buffer);
-    sem_up(s_item);
+    item = malloc(sizeof(item_t));
+    item->prev = NULL;
+    item->next=NULL;
+    item->valor = rand()%99;
+
+    sem_down(&s_vaga);
+
+    sem_down(&s_buffer);
+    queue_append((queue_t**)&buffer,(queue_t*)item);
+    printf("%s produziu %d\n",(char*)arg,item->valor);
+    sem_up(&s_buffer);
+
+    sem_up(&s_item);
   }
+  task_exit(0);
 }
 
-void consumidor(){
-  while(1){
-    sem_down(s_item);
-    sem_down(s_buffer);
-
-    sem_up(s_buffer);
-    sem_up(s_vaga);
+void consumidor(void *arg){
+  item_t *item;
+  int i;
+  printf("%s início\n",(char*)arg);
+  for(i=0;i<NUMSTEPS/2;i++) {
+    sem_down(&s_item);
+    sem_down(&s_buffer);
+    item = (item_t*) queue_remove((queue_t**)&buffer,(queue_t*)buffer);
+    printf("%s consumiu %d\n",(char*)arg,item->valor);
+    free(item);
+    sem_up(&s_buffer);
+    sem_up(&s_vaga);
 
     task_sleep(1);
   }
+  task_exit(0);
 }
 
 int main(){
-  bufferIndex = 0;
-  sem_create(s_buffer,5);
-  sem_create(s_vaga,5);
-  sem_create(s_item,0);
+  pingpong_init();
+  printf("MAIN início\n");
 
-  sem_destroy(s_buffer);
-  sem_destroy(s_vaga);
-  sem_destroy(s_item);
-  return(0);
+  sem_create(&s_buffer,5);
+  sem_create(&s_vaga,5);
+  sem_create(&s_item,0);
+
+  task_create(&p1,produtor,"p1");
+  task_create(&p2,produtor,"p2");
+  task_create(&p3,produtor,"p3");
+  task_create(&c1,consumidor,"        c1");
+  task_create(&c2,consumidor,"        c2");
+
+  task_join(&p1);
+  task_join(&p2);
+  task_join(&p3);
+  task_join(&c1);
+  task_join(&c2);
+
+  sem_destroy(&s_buffer);
+  sem_destroy(&s_vaga);
+  sem_destroy(&s_item);
+  task_exit(0);
+  exit(0);
 }
